98efc9f56138d8d34cb553cd7c876acf890e90ec
Lucien Gaitskell <lucien@gaitskell.com>
2015-02-03 16:13:54 -0500

    Edits...

---
+++ website/cgi-bin/index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+#import cgitb
+#cgitb.enable()
+
+print "Content-type: text/html"
+print
+print "<html><head><title>Test</title></head>"
+print "<body>"
+print "hello cgi"
+print "</body>"
+print "</html>"
+
+#print "Content-Type: text/plain;charset=utf-8"
+#print
+
+#print "Hello World!"
