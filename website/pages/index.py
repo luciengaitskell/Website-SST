aa171643c767f5bc0be375c669758e3f8b92c4c1
Lucien Gaitskell <lucien@gaitskell.com>
2015-02-05 20:06:20 -0500

    Implemented a Text File Read Draft #1

--- website/pages/index.py
+++ website/pages/index.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from file_len import file_length
 import glob
 
 @app.route('/articles/<articleNumber>/')
 def main(articleNumber=None):
-	fileBeginning="article-"
-	fileExtention=".txt"
-	fileName= str(fileBeginning) + str(articleNumber) + str(fileExtention)
-		
+	if articleNumber!=None:
+		fileSubfolder="articles/"
+		fileBeginning="article-"
+		fileExtention=".txt"
+		filePath=str(fileSubfolder) + str(fileBeginning) + str(articleNumber) + str(fileExtention)
+
+		fileIsThere=False
+		for name in glob.glob(filePath):
+	    	fileIsThere=True
+
+		if fileIsThere==False:
+			redirect(/article, code=302)
+
+
+		#fileSize = file_lenght(filePath) #shouldn't need this because the output of readlines is an array
+		return render_template('article-render.html', filePath=filePath, fileSize=fileSize)
+	else:
+		return "THIS IS NOT THE FILE YOUR LOOKING FOR"
+
 
 
 if __name__ == "__main__":
