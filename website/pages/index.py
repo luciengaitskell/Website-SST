9e7870e68ab52d7f2195c4e87bd1c4fbbefa8ebd
Lucien Gaitskell <lucien@gaitskell.com>
2015-02-08 21:23:46 -0500

    test

--- website/pages/index.py
+++ website/pages/index.py
@@ -76,7 +76,7 @@ def displayMain():
 			newText=[jj[3],jj[4]]
 			if len(newText[0]) > maxLineLength:
 				newText[0]=(newText[0])[:len(newText[0])-maxLineLength]
-				return len(newText[0])-maxLineLength
+				#return len(newText[0])-maxLineLength
 				newText[1]=(newText[0])[maxLineLength:]+str(newText[1])
 				newText[1]=(newText[1])[:len(newText[1])-maxLineLength]+str("...")
 				texts.append(newText[0])
