d009dab0b14d564889d788538f6486a3b940b5fc
Lucien Gaitskell <lucien@gaitskell.com>
2015-02-16 14:26:28 -0500

    test

--- website/pages/index.py
+++ website/pages/index.py
@@ -66,7 +66,7 @@ def signOut():
 
 @app.route('/test/')
 def testFunc():
-	return str(url_for(base_url))
+	return str(url_for(path))
 """def autoLoginTest():
 	credsCorrect=loginCheck(False,-1,userPass)
 	#return session['username']
@@ -348,7 +348,6 @@ def signature():
 
 
 if __name__ == "__main__":
-	app.config['SERVER_NAME']="lucien.ddns.net/"
 	app.secret_key = 'Ymsf,sfatwBU!Iwruh,bus'
 	app.debug = True
 	app.run(
