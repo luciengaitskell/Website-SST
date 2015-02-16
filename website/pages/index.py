88e5e712183ae9124d0f397b18a0e6b302cc2d72
Lucien Gaitskell <lucien@gaitskell.com>
2015-02-16 14:36:59 -0500

    test

--- website/pages/index.py
+++ website/pages/index.py
@@ -66,7 +66,7 @@ def signOut():
 
 @app.route('/test/')
 def testFunc():
-	return str(url_for(path))
+	return request.path
 """def autoLoginTest():
 	credsCorrect=loginCheck(False,-1,userPass)
 	#return session['username']
