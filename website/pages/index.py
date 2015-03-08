8261170bd9fa9650ea4824fba679c5416483f85f
Lucien Gaitskell <lucien@gaitskell.com>
2015-03-08 13:27:35 -0400

    test

--- website/pages/index.py
+++ website/pages/index.py
@@ -209,6 +209,7 @@ def displayMain():
 
 			newText=[(jj[3].decode("utf-8")),""]
 
+			return newText[0]
 			if len(newText[0] + unicode(" "))-1 > maxLineLength: # if the first display line is over max
 				newText[0]=(newText[0])[:maxLineLength]
 				newText[1]=(jj[3].decode("utf-8"))[maxLineLength:]
