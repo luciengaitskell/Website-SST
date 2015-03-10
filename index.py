e52c6aa4818e7084bc7785d79255979a111b0ac4
Lucien Gaitskell <lucien@gaitskell.com>
2015-03-10 07:47:40 -0400

    adapting the test server

--- index.py
+++ index.py
@@ -4,6 +4,8 @@ from datetime import timedelta
 import time
 import glob
 
+#for dev - change port and debug afterwards
+
 app = Flask(__name__)
 
 userPass=[["~owner", "a_boss"]]
@@ -514,5 +516,5 @@ if __name__ == "__main__":
 	app.debug = True
 	app.run(
 		host='0.0.0.0',
-		port=80
+		port=5000
 		)
