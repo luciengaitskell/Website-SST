1cdc4a2f0321295a9ab3a75c1bcdc657ef2eefcf
Lucien Gaitskell <lucien@gaitskell.com>
2015-03-18 16:02:20 -0400

    test

--- index.py
+++ index.py
@@ -240,7 +240,7 @@ def displayMain():
 	#return "fileNames: " +str(fileNames) +" | dates: " + str(dates)
 	if len(fileNames)>0: # there are files
 
-		fileNamesSorted=fileDateNumbOrgainise(dates, fileNames, fileBeginning, fileExtention)
+		fileNamesSorted=fileDateNumbOrgainise(dates, fileNames, fileSubFolder +fileBeginning, fileExtention)
 		return str(fileNamesSorted)
 		# getting infor in the sorted order
 		for ii in fileNamesSorted:
