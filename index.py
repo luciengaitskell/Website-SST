c61cbdd1b70b3029cd568f68f399b082345e811a
Lucien Gaitskell <lucien@gaitskell.com>
2015-03-18 15:32:48 -0400

    test

--- index.py
+++ index.py
@@ -238,7 +238,7 @@ def displayMain():
 	if len(fileNames)>0: # there are files
 
 		fileNamesSorted=fileDateNumbOrgainise(dates, fileNames, fileBeginning, fileExtention)
-		return fileNamesSorted
+		return str(fileNamesSorted)
 		# getting infor in the sorted order
 		for ii in fileNamesSorted:
 			fileNameOpen = open((str(fileSubFolder) + str(ii)), "r")
