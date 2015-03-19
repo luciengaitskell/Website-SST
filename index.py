e0e4461678ec66e0760ce18464e7f7df3a596eae
Lucien Gaitskell <lucien@gaitskell.com>
2015-03-19 17:04:32 -0400

    test

--- index.py
+++ index.py
@@ -130,8 +130,9 @@ def fileDateNumbOrgainise(dates, fileNames, theFileBeginning, theFileExtention):
 
 		for jj in fileNames:
 			#return "jj: " + jj + " | returned: " + str(findBetween(jj, "-", len(theFileBeginning)))
-			return (jj)[len(theFileBeginning):findBetween(jj, "-", len(theFileBeginning))]
+			#return (jj)[len(theFileBeginning):findBetween(jj, "-", len(theFileBeginning))]
 			if uniqueDates[ii] == (jj)[len(theFileBeginning):findBetween(jj, "-", theFileBeginning)]:
+				return "u should see dis"
 				fileNamesNew.append((jj)[len(theFileBeginning):len(jj)-len(theFileExtention)])
 
 		fileNamesNew.sort(reverse=True)
