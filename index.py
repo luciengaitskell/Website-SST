ebfa470e8f0463093e7b6e996177f2e35d5ec88a
Lucien Gaitskell <lucien@gaitskell.com>
2015-03-19 17:02:55 -0400

    Revert "etst"
    This reverts commit a5c94a7238e6c26512ad4e7adcf907ff52f73546.

--- index.py
+++ index.py
@@ -129,6 +129,8 @@ def fileDateNumbOrgainise(dates, fileNames, theFileBeginning, theFileExtention):
 		fileNamesNew=[]
 
 		for jj in fileNames:
+			return "jj: " + jj + " | returned: " + str(findBetween(jj, "-", len(theFileBeginning)))
+			return (jj)[len(theFileBeginning):findBetween(jj, "-", len(theFileBeginning))]
 			if uniqueDates[ii] == (jj)[len(theFileBeginning):findBetween(jj, "-", theFileBeginning)]:
 				fileNamesNew.append((jj)[len(theFileBeginning):len(jj)-len(theFileExtention)])
 
