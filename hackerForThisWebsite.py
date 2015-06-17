8fdce3c1dd128c03885442edb92dff9ebb3a0d7f
Lucien Gaitskell <lucien@gaitskell.com>
2015-06-16 21:34:07 -0400

    added termination statement

--- hackerForThisWebsite.py
+++ hackerForThisWebsite.py
@@ -7,6 +7,7 @@ while True:
     r = requests.post("http://lucieng.ddns.net:5000/loginCheck", data=payload)
     # data is used instead of params because it is url encoded
     if r.text[80:81]==u'e':
+        print("WE DID IT JIM")
         break
     elif r.text[80:81]!=u'o':
         print("something weird is going on")
