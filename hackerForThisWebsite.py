26a78e9f607129d8cfbe6a513028bc595d761822
Lucien Gaitskell <lucien@gaitskell.com>
2015-06-16 22:51:55 -0400

    commented out old main thread

--- hackerForThisWebsite.py
+++ hackerForThisWebsite.py
@@ -19,3 +19,22 @@ while True:
         break
     elif r.text[80:81]!=u'o':
         print("something weird is going on")
+'''
+if __name__ == "__main__":
+    asciiChars=string.printable
+    password=[]
+    maxPasswordLen=10
+    while True:
+        while len(password)<maxPasswordLen:
+            password.append("")
+            for curChar in asciiChars:
+
+            payload = {'username':'~owner','password':'a_boss'}
+            r = requests.post("http://lucieng.ddns.net:5000/loginCheck", data=payload)
+            # data is used instead of params because it is url encoded
+            if r.text[80:81]==u'e':
+                print("WE DID IT JIM")
+                break
+            elif r.text[80:81]!=u'o':
+                print("something weird is going on")
+'''
