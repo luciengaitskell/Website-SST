01868460d7cc5d1a955a2604f813ee295ac34f62
Lucien Gaitskell <lucien@gaitskell.com>
2015-06-16 21:32:15 -0400

    turning "=" into "==" :)

--- hackerForThisWebsite.py
+++ hackerForThisWebsite.py
@@ -6,7 +6,7 @@ while True:
 
     r = requests.post("http://lucieng.ddns.net:5000/loginCheck", data=payload)
     # data is used instead of params because it is url encoded
-    if r.text[80:81]=u'e':
+    if r.text[80:81]==u'e':
         break
-    elif r.text[80:81]!=u'o':
+    elif r.text[80:81]!==u'o':
         print("something weird is going on")
